56950eb472bdbb72ebc27b1b3215b81fafe28ecd
Micael Azarias <azazmica10@gmail.com>
2023-11-20 15:11:33 -0300

    git

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ button.addEventListener(("click"), function(event) {
 
   clearForm();
 });
+
+
+// const box = document.querySelector(".container");
+// const imagens = document.querySelectorAll(".container img");
+
+// let contador = 0;
+
+// function slider() {
+//     contador++;
+
+//     if (contador > imagens.length - 1) {
+//         contador = 0;
+//     }
+
+//     box.style.transform = ` translateX(${-contador * 1250}px)`
+// }
+// setInterval(slider , 2000);
